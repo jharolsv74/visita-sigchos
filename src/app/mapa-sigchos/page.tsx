eaac65184a7c2468e461896da99d9fb3bc1819a0
jharolsv74 <jsuchuari@espe.edu.ec>
2025-09-04 01:07:00 -0500

    Resolving Conflicts

--- src/app/mapa-sigchos/page.tsx
+++ src/app/mapa-sigchos/page.tsx
@@ -162,7 +162,7 @@ export default function MapaSigchos() {
                 </p>
                 <button
                   onClick={() => {
-                    const mapSection = document.getElementById('map-section');
+                    const mapSection = document.getElementById('mapa-container');
                     mapSection?.scrollIntoView({ behavior: 'smooth' });
                   }}
                   className="mapa-btn py-4 px-8 bg-transparent border-2 border-white text-white rounded-full hover:bg-white hover:text-[#12141d] transition-all duration-300 text-lg font-semibold">
@@ -172,7 +172,7 @@ export default function MapaSigchos() {
               <div className="flex justify-end">
                 <button
                   onClick={() => {
-                    const mapSection = document.getElementById('map-section');
+                    const mapSection = document.getElementById('mapa-container');
                     mapSection?.scrollIntoView({ behavior: 'smooth' });
                   }}
                   className="absolute bottom-24 right-24 w-16 h-16 rounded-full border-2 border-white flex items-center justify-center hover:bg-white hover:text-[#12141d] text-white transition-all duration-300"
@@ -187,29 +187,6 @@ export default function MapaSigchos() {
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth={2}
-                      d="M19 14l-7 7m0 0l-7-7m7 7V3"
-                    />
-                  </svg>
-                </button>
-              </div>
-              <div className="flex justify-end">
-                <button 
-                  onClick={() => {
-                    const formSection = document.getElementById('mapa-sigchos');
-                    formSection?.scrollIntoView({ behavior: 'smooth' });
-                  }}
-                  className="absolute bottom-24 right-24 w-16 h-16 rounded-full border-2 border-white flex items-center justify-center hover:bg-white hover:text-[#12141d] text-white transition-all duration-300"
-                >
-                  <svg 
-                    className="w-8 h-8" 
-                    fill="none" 
-                    stroke="currentColor" 
-                    viewBox="0 0 24 24"
-                  >
-                    <path 
-                      strokeLinecap="round" 
-                      strokeLinejoin="round" 
-                      strokeWidth={2} 
                       d="M19 14l-7 7m0 0l-7-7m7 7V3"
                     />
                   </svg>
